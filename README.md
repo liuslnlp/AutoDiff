a387470b20af9549231397735f00129b57717973
wisedoge <wisedoge@outlook.com>
2018-01-18 20:02:19 +0800

    Beta v0.0

--- README.md
+++ README.md
@@ -3,4 +3,41 @@ A C++ library of automatic differentiation based on static computational graph.
 
 ## Demo
 
-See `main.cpp`.
+```cpp
+#include <AutoDiff/autodiff.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void test_backward() {
+    ad::Node x1 = ad::Variable("x1");
+    ad::Node x2 = ad::Variable("x2");
+    ad::Node y = 3 * x1 + 4 * x2 + 5;
+    auto t = Gradients(y, vector<ad::Node>{x1, x2});
+    ad::Executor e(vector<ad::Node> { y, t[0], t[1] });
+    ad::FeedDict feed_dict;
+    vector<ad::Ctype> ans;
+    feed_dict[x1] = 5;
+    feed_dict[x2] = 5;
+    ans = e.run(feed_dict);
+
+    cout << "y: " << ans[0] << endl;
+    cout << "dy/dx1: " << ans[1] << endl;
+    cout << "dy/dx2: " << ans[2] << endl;
+
+    feed_dict[x1] = 2;
+    feed_dict[x2] = 6;
+    ans = e.run(feed_dict);
+
+    cout << "y: " << ans[0] << endl;
+    cout << "dy/dx1: " << ans[1] << endl;
+    cout << "dy/dx2: " << ans[2] << endl;
+}
+
+int main() {
+    test_backward();
+    return 0;
+}
+```
--- demo.cpp
+++ demo.cpp
@@ -1,40 +1,35 @@
 #include <AutoDiff/autodiff.h>
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
-using ad::Node;
-using ad::Ctype;
-
 
 void test_forward() {
-    Node x = ad::Variable("x");
-    Node y = x + 3;
-    Node z = y + 2;
-    Node zz = x + y + z;
+    ad::Node x = ad::Variable("x");
+    ad::Node y = x + 3;
+    ad::Node z = y + 2;
+    ad::Node zz = x + y + z;
 
-    vector<Node> vals{x, y, z, zz};
+    vector<ad::Node> vals{x, y, z, zz};
     ad::Executor e(vals);
     ad::FeedDict feed_dict;
     feed_dict[x] = 5;
-    vector<Ctype> ans = e.run(feed_dict);
-    for (auto &i : ans) {
-        cout << i << " ";
-    }
-    cout << endl;
-    cout << y.name << endl;
-    cout << z.name << endl;
-    cout << zz.name << endl;
+    vector<ad::Ctype> ans = e.run(feed_dict);
+
+    cout << y.name << "=" <<ans[1]<<endl;
+    cout << z.name << "=" <<ans[2]<<endl;
+    cout << zz.name << "=" <<ans[3]<<endl;
 }
 
 void test_backward() {
-    Node x1 = ad::Variable("x1");
-    Node x2 = ad::Variable("x2");
-    Node y = 3 * x1 + 4 * x2 + 5;
-    auto t = Gradients(y, vector<Node>{x1, x2});
-    ad::Executor e(vector<Node> { y, t[0], t[1] });
+    ad::Node x1 = ad::Variable("x1");
+    ad::Node x2 = ad::Variable("x2");
+    ad::Node y = 3 * x1 + 4 * x2 + 5;
+    auto t = Gradients(y, vector<ad::Node>{x1, x2});
+    ad::Executor e(vector<ad::Node> { y, t[0], t[1] });
     ad::FeedDict feed_dict;
-    vector<Ctype> ans;
+    vector<ad::Ctype> ans;
     feed_dict[x1] = 5;
     feed_dict[x2] = 5;
     ans = e.run(feed_dict);
@@ -53,7 +48,7 @@ void test_backward() {
 }
 
 int main() {
+    test_forward();
     test_backward();
-
     return 0;
 }
